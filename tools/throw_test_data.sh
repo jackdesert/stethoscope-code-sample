aa06968efb49497c50bcf6fb988435d2377db263
Jack Desert <jdtzn@riseup.net>
2018-10-11 11:25:32 -0500

    Updated badge ids in fake thrown data

--- tools/throw_test_data.sh
+++ tools/throw_test_data.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 for i in {1..10000}; do
-    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"a": -30, "b": -35, "c": -40, "d": -42, "e": -50} }'
+    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"BIP003": -30, "BIP010": -35, "BIP020": -40, "BIP022": -42, "BIP036": -50} }'
     sleep 1
 
-    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"a": -34, "b": -38, "c": -52, "d": -53, "e": -54} }'
+    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"BIP003": -34, "BIP010": -38, "BIP020": -52, "BIP022": -53, "BIP036": -54} }'
     sleep 1
 
-    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"a": -34, "b": -37, "c": -45, "d": -60, "e": -63} }'
+    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"BIP003": -34, "BIP010": -37, "BIP020": -45, "BIP022": -60, "BIP036": -63} }'
     sleep 1
 
-    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"a": -41, "b": -42, "c": -52, "d": -60, "e": -63} }'
+    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"BIP003": -41, "BIP010": -42, "BIP020": -52, "BIP022": -60, "BIP036": -63} }'
     sleep 1
 
-    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"a": -39, "b": -42, "c": -62, "d": -70, "e": -63} }'
+    curl -X POST -H "Content-Type:application/json"  -i http://localhost:6540/rssi_readings   -d '{"badge_id":"2", "pi_id":"2", "beacons":{"BIP003": -39, "BIP010": -42, "BIP020": -62, "BIP022": -70, "BIP036": -63} }'
     sleep 1
 done
 
