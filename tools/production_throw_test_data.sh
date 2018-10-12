ff88e36c61a194f3b59c6c7b572723f02db0cabc
Jack Desert <jdtzn@riseup.net>
2018-10-12 11:13:16 -0500

    Fixed production_throw_test_data

--- tools/production_throw_test_data.sh
+++ tools/production_throw_test_data.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 for i in {1..10000}; do
-    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"BIP003": -31, "BIP010": -36, "BIP020": -42, "BIP022": -57, "BIP036": -58} }'
+    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"BIP003": -31, "BIP010": -36, "BIP020": -42, "BIP022": -57, "BIP036": -58} } '
     sleep 1
 
 
-    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"BIP003": -52, "BIP010": -16, "BIP020": -17, "BIP022": -27, "BIP036": -42} }'
+    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"BIP003": -52, "BIP010": -16, "BIP020": -17, "BIP022": -27, "BIP036": -42} }'
     sleep 1
 
-    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"BIP003": -18, "BIP010": -31, "BIP020": -49, "BIP022": -47, "BIP036": -40} }'
+    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"BIP003": -18, "BIP010": -31, "BIP020": -49, "BIP022": -47, "BIP036": -40} }'
     sleep 1
 
-    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"BIP003": -40, "BIP010": -45, "BIP020": -30, "BIP022": -22, "BIP036": -35} }'
+    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"BIP003": -40, "BIP010": -45, "BIP020": -30, "BIP022": -22, "BIP036": -35} }'
     sleep 1
 
-    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"BIP003": -32, "BIP010": -45, "BIP020": -50, "BIP022": -82, "BIP036": -42} }'
+    curl -k -X POST -H "Content-Type:application/json"  -i https://bip-stethoscope.elitecare.com/rssi_readings   -d '{"badge_id":"2", "pi_id":"1", "beacons":{"BIP003": -32, "BIP010": -45, "BIP020": -50, "BIP022": -82, "BIP036": -42} }'
     sleep 1
 done
 
